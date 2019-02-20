be638e7c5b7eaa40923858e710392af3f4b9d18d
deepok1@yahoo.com <deepok1@yahoo.com>
2019-02-20 08:37:31 +0200

    Updates

---
+++ precache-manifest.2b799f8ce81c744f8f839150003cb2c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0408d09ad9a640b0fbe2",
+    "url": "/eStoc/static/js/runtime~main.0408d09a.js"
+  },
+  {
+    "revision": "5e89f631e7b85c86576d",
+    "url": "/eStoc/static/js/main.5e89f631.chunk.js"
+  },
+  {
+    "revision": "dc8e56505480073b8bca",
+    "url": "/eStoc/static/js/1.dc8e5650.chunk.js"
+  },
+  {
+    "revision": "5e89f631e7b85c86576d",
+    "url": "/eStoc/static/css/main.f714bf5d.chunk.css"
+  },
+  {
+    "revision": "6dc70fcb6b3a060273e679d4292e924e",
+    "url": "/eStoc/index.html"
+  }
+];
